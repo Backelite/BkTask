b8e91d0e0b429cd4531ebb81765080a991efb8c0
gaspard.viot <gaspard.viot@2d44191c-5f22-0410-8a2c-f87add29a577>
2013-08-28 13:11:00 +0000

    - Added a common header for Task module

---
+++ BkTask/Classes/BkTaskCore.h
@@ -0,0 +1,21 @@
+//
+//  BkTaskCore.h
+//  BkTask
+//
+//  Created by Gaspard Viot on 28/08/13.
+//  Copyright (c) 2013 Backelite. All rights reserved.
+//
+
+//Task
+#import "BkTask.h"
+#import "BkTaskObserver.h"
+#import "BkTaskContent.h"
+#import "BkTask+BkNetwork.h"
+
+//Steps
+#import "BkBasicStepOperation.h"
+#import "BkBlockStepOperation.h"
+#import "BkFileSavingOperation.h"
+#import "BkFileLoadingOperation.h"
+#import "BkURLRequestOperation.h"
+#import "BkJSONParsingOperation.h"
