e57820b3283bceb8b52f95cccdb31aa78883dc33
Gaspard Viot <gaspard.viot@backelite.com>
2013-09-09 13:20:10 +0200

    Added MIT license text to each file

--- BkTask/Classes/BkLog.h
+++ BkTask/Classes/BkLog.h
@@ -1,10 +1,25 @@
 //
-//  BkLog.h
-//  BkCommon
+// The MIT License (MIT)
 //
-//  Created by Thomas SARLANDIE on 07/10/08.
-//  Copyright 2008 Backelite. All rights reserved.
+// Copyright (c) 2013 Backelite
 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 #import <Foundation/Foundation.h>
 
@@ -67,11 +82,6 @@
  *
  * Use BKLogE, BKLogW and BKLogD macros to log at different levels (Error, Warning, Debug).
  * Error logs will be prefixed by "!! ", Warning logs by "WW ", and Debug logs are displayed as is.
- *
- * @author Thomas Sarlandie
- * @author Philippe Bernery
- * @author Pierre Monod-Broca
- * @author Marco Nikic
  *
  * @ingroup Utilities
  */
